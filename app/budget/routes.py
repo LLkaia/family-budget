7edb3213b5aea0ac1a115b6324817e5e6f7eba12
Illia Kaialainien <ikaialai@cisco.com>
2024-09-14 10:27:01 +0300

    predefined categories

--- app/budget/routes.py
+++ app/budget/routes.py
@@ -3,10 +3,10 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-from budget.crud import create_budget_with_user
-from budget.models import Budget, BudgetBase
+from budget.crud import create_budget_with_user, create_predefined_category, get_predefined_categories
+from budget.models import Budget, BudgetBase, CategoryBase, PredefinedCategories, PredefinedCategory
 from core.database import get_db
-from users.auth import current_user
+from users.auth import current_superuser, current_user
 from users.models import User
 
 
@@ -20,3 +20,21 @@ async def create_budget(
     """Create new budget for current user."""
     budget = await create_budget_with_user(session, budget, user)
     return budget
+
+
+@router.post("/predefined-categories", response_model=PredefinedCategory, dependencies=[Depends(current_superuser)])
+async def create_predefined_categories(
+    category: CategoryBase, session: Annotated[AsyncSession, Depends(get_db)]
+) -> PredefinedCategory:
+    """Create new predefined category."""
+    category = await create_predefined_category(session, category)
+    return category
+
+
+@router.get("/predefined-categories", response_model=PredefinedCategories, dependencies=[Depends(current_user)])
+async def list_predefined_categories(
+    session: Annotated[AsyncSession, Depends(get_db)], offset: int = 0, limit: int = 100
+) -> PredefinedCategories:
+    """Retrieve predefined category."""
+    categories = await get_predefined_categories(session, offset, limit)
+    return categories
