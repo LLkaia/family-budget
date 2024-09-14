657b172ec0f349799384864fe9357f308ed39b25
Illia Kaialainien <ikaialai@cisco.com>
2024-09-14 11:57:54 +0300

    get budget route with user validation

--- app/budget/routes.py
+++ app/budget/routes.py
@@ -3,7 +3,12 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-from budget.crud import create_budget_with_user, create_predefined_category, get_predefined_categories
+from budget.crud import (
+    create_budget_with_user,
+    create_predefined_category,
+    get_budget_by_id_with_current_user,
+    get_predefined_categories,
+)
 from budget.models import Budget, BudgetBase, CategoryBase, PredefinedCategories, PredefinedCategory
 from core.database import get_db
 from users.auth import current_superuser, current_user
@@ -13,12 +18,18 @@ from users.models import User
 router = APIRouter()
 
 
-@router.post("/create", response_model=Budget)
+@router.post("/", response_model=Budget)
 async def create_budget(
     budget: BudgetBase, session: Annotated[AsyncSession, Depends(get_db)], user: Annotated[User, Depends(current_user)]
 ) -> Budget:
     """Create new budget for current user."""
     budget = await create_budget_with_user(session, budget, user)
+    return budget
+
+
+@router.get("/{budget_id}", response_model=Budget)
+async def get_budget(budget: Annotated[Budget, Depends(get_budget_by_id_with_current_user)]) -> Budget:
+    """Get budget by id."""
     return budget
 
 
