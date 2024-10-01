963279c86030efefadc8f9d7790106d665842997
Illia Kaialainien <ikaialai@cisco.com>
2024-10-01 20:14:47 +0300

    update path parameters

--- app/users/routes.py
+++ app/users/routes.py
@@ -16,13 +16,13 @@ from users.schemas import Message, Token, UserCreate, UserDetails, UserList
 router = APIRouter()
 
 
-@router.get("/", response_model=UserDetails)
+@router.get("/", response_model=UserDetails, response_model_exclude_none=True)
 async def get_me_detailed(user: Annotated[User, Depends(current_user)]) -> User:
     """Get current user info."""
     return user
 
 
-@router.get("/users", response_model=UserList, dependencies=[Depends(current_superuser)])
+@router.get("/users", dependencies=[Depends(current_superuser)])
 async def get_list_of_users(
     session: Annotated[AsyncSession, Depends(get_db)], offset: int = 0, limit: int = 100
 ) -> UserList:
@@ -31,7 +31,7 @@ async def get_list_of_users(
     return users
 
 
-@router.post("/login", response_model=Token)
+@router.post("/login")
 async def login_for_access_token(
     session: Annotated[AsyncSession, Depends(get_db)], form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
 ) -> Token:
@@ -45,13 +45,13 @@ async def login_for_access_token(
     return Token(access_token=access_token, token_type="bearer")
 
 
-@router.post("/logout", response_model=Message, dependencies=[Depends(destroy_token)])
+@router.post("/logout", dependencies=[Depends(destroy_token)])
 async def logout_and_destroy_token() -> Message:
     """Logout user."""
     return Message(message="Successfully logged out.")
 
 
-@router.post("/register", response_model=Message)
+@router.post("/register", status_code=status.HTTP_201_CREATED)
 async def register_new_user(session: Annotated[AsyncSession, Depends(get_db)], user: UserCreate) -> Message:
     """Register new user."""
     try:
@@ -61,7 +61,7 @@ async def register_new_user(session: Annotated[AsyncSession, Depends(get_db)], u
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered.")
 
 
-@router.post("/verify-token", response_model=Message, dependencies=[Depends(current_user)])
+@router.post("/verify-token", dependencies=[Depends(current_user)])
 def test_token() -> Message:
     """Verify user token."""
     return Message(message="Token is valid.")
