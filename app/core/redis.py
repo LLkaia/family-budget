417ce42eab71d59ecfd6ea33e6836210bc86435f
Illia Kaialainien <ikaialai@cisco.com>
2024-11-26 10:21:20 +0200

    write through pattern

--- app/core/redis.py
+++ app/core/redis.py
@@ -1,19 +1,24 @@
 import uuid
 from datetime import timedelta
 from enum import Enum
+from functools import wraps
+from typing import Any, Awaitable, Callable, TypeVar
 
 import redis.asyncio as redis  # type: ignore[import-untyped]
+from sqlmodel import SQLModel
 
 from core.config import get_settings
 
 
 config = get_settings()
+T = TypeVar("T", bound=SQLModel)
 
 
 class RedisKeys(str, Enum):
     """Redis Keys Identifiers."""
 
     blacklist = "blacklist"
+    predefined_category = "predefined_category"
 
 
 class RedisClient:
@@ -25,11 +30,34 @@ class RedisClient:
 
     async def add_token_to_blacklist(self, jwt_id: uuid.UUID, ttl: timedelta) -> None:
         """Add token to blacklist."""
-        await self._redis.setex(f"{RedisKeys.blacklist}:{jwt_id}", ttl, "blacklisted")
+        await self._redis.setex(f"{RedisKeys.blacklist.value}:{jwt_id}", ttl, "blacklisted")
 
     async def is_token_blacklisted(self, jwt_id: uuid.UUID) -> bool:
         """Check if token is blacklisted."""
-        return bool(await self._redis.exists(f"{RedisKeys.blacklist}:{jwt_id}") > 0)
+        return bool(await self._redis.exists(f"{RedisKeys.blacklist.value}:{jwt_id}") > 0)
+
+    async def add_row_to_cache(self, redis_key: str, key: str, value: str) -> None:
+        """Add row to cache."""
+        await self._redis.set(f"{redis_key}:{key}", value)
+
+    async def read_row_from_cache(self, redis_key: str, key: str) -> Any:
+        """Read row from cache."""
+        return await self._redis.get(f"{redis_key}:{key}")
 
 
 redis_client = RedisClient(host=config.redis_host, port=config.redis_port)
+
+
+def write_through_cache(redis_key: str) -> Callable[[Callable[..., Awaitable[T]]], Callable[..., Awaitable[T]]]:
+    """Write SQL row to redis cache."""
+
+    def inner(func: Callable[..., Awaitable[T]]) -> Callable[..., Awaitable[T]]:
+        @wraps(func)
+        async def wrapper(*args: tuple[Any, ...], **kwargs: dict[str, Any]) -> T:
+            result = await func(*args, **kwargs)
+            await redis_client.add_row_to_cache(redis_key, result.id, result.model_dump_json())
+            return result
+
+        return wrapper
+
+    return inner
