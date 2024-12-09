8944ef93f5a0ee976febef972dd563e80d613779
Illia Kaialainien <ikaialai@cisco.com>
2024-12-09 21:14:58 +0200

    added basic routes

--- app/stocks/schemas.py
+++ app/stocks/schemas.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from sqlmodel import Field, SQLModel
+
 
 class StockTransactionType(str, Enum):
     """Stock Transaction Types Enum."""
@@ -14,3 +16,10 @@ class AccountTransactionType(str, Enum):
     DIVIDENDS = "dividends"
     DEPOSIT = "deposit"
     WITHDRAWAL = "withdrawal"
+
+
+class StockAccountCreate(SQLModel):
+    """Stock Account Create schema."""
+
+    balance: float = Field(ge=0, description="Current account balance.")
+    account_name: str = Field(max_length=255, description="Account name.")
